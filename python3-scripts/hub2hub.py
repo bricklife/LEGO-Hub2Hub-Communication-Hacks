d232f1f8ef3e12d06244ff564f3964d87cfcea65
Shinichiro Oba <bricklife@gmail.com>
2021-09-01 11:58:16 +0900

    Define OGF and OCF values

--- python3-scripts/hub2hub.py
+++ python3-scripts/hub2hub.py
@@ -3,20 +3,26 @@ import binascii
 import struct
 import time
 
+OGF_LE_CTL = 0x08
+
+OCF_LE_SET_ADVERTISING_PARAMETERS = 0x0006
+OCF_LE_SET_ADVERTISING_DATA = 0x0008
+OCF_LE_SET_ADVERTISE_ENABLE = 0x000a
+
 def _set_advertising_parameters(sock, interval):
     interval = interval & 0xffff
     type = 0x02 # ADV_SCAN_IND
     param = struct.pack('<HHBBB6sBB', interval, interval, type, 0, 0, b'', 7, 0)
-    bluez.hci_send_cmd(sock, 0x08, 0x0006, param)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_ADVERTISING_PARAMETERS, param)
 
 def _set_advertising_data(sock, data):
     data = data[:31]
     param = bytes([len(data)]) + data
-    bluez.hci_send_cmd(sock, 0x08, 0x0008, param)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_ADVERTISING_DATA, param)
 
 def _set_advertise_enable(sock, enable):
     param = b'\x01' if enable else b'\x00'
-    bluez.hci_send_cmd(sock, 0x08, 0x000a, param)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_ADVERTISE_ENABLE, param)
 
 def transmit_signal(transmission_id, signal, value, interval = 0x00a0, duration = 0.5):
     transmission_id = transmission_id & 0xff
