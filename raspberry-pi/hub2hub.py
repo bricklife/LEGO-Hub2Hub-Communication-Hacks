10fb0ce6337ac92904330e9a5c1b6d3d1bb8cd9e
Shinichiro Oba <bricklife@gmail.com>
2021-09-01 20:58:03 +0900

    Add receive_signal()

--- raspberry-pi/hub2hub.py
+++ raspberry-pi/hub2hub.py
@@ -5,15 +5,22 @@ import time
 
 OGF_LE_CTL = 0x08
 
-OCF_LE_SET_ADVERTISING_PARAMETERS = 0x0006
-OCF_LE_SET_ADVERTISING_DATA = 0x0008
-OCF_LE_SET_ADVERTISE_ENABLE = 0x000a
+OCF_LE_SET_ADVERTISING_PARAMETERS   = 0x0006
+OCF_LE_SET_ADVERTISING_DATA         = 0x0008
+OCF_LE_SET_ADVERTISE_ENABLE         = 0x000a
+OCF_LE_SET_SCAN_PARAMETERS          = 0x000b
+OCF_LE_SET_SCAN_ENABLE              = 0x000c
+
+EVT_LE_META_EVENT           = 0x3e
+EVT_LE_ADVERTISING_REPORT   = 0x02
+
+ADV_SCAN_IND = 0x02
 
 def _set_advertising_parameters(sock, interval):
     interval = interval & 0xffff
     if interval < 0x00a0:
         interval = 0x00a0 # shall not be set to less than 0x00A0 (100 ms) if the Advertising_Type is set to 0x02 (ADV_SCAN_IND) 
-    type = 0x02 # ADV_SCAN_IND
+    type = ADV_SCAN_IND
     param = struct.pack('<HHBBB6sBB', interval, interval, type, 0x00, 0x00, b'', 0b00000111, 0x00)
     bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_ADVERTISING_PARAMETERS, param)
 
@@ -25,6 +32,15 @@ def _set_advertising_data(sock, data):
 def _set_advertise_enable(sock, enable):
     param = b'\x01' if enable else b'\x00'
     bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_ADVERTISE_ENABLE, param)
+
+def _set_scan_parameters(sock):
+    param = struct.pack('<BHHBB', 0x00, 0x0010, 0x0010, 0x00, 0x00)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_SCAN_PARAMETERS, param)
+
+def _set_scan_enable(sock, enable):
+    le_scan_enable = 0x01 if enable else 0x00
+    param = struct.pack('BB', le_scan_enable, 0x00)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_SCAN_ENABLE, param)
 
 def transmit_signal(transmission_id, signal, value, interval = 0x00a0, duration = 0.5):
     transmission_id = transmission_id & 0xff
@@ -39,6 +55,48 @@ def transmit_signal(transmission_id, signal, value, interval = 0x00a0, duration 
     time.sleep(duration)
     _set_advertise_enable(sock, False)
 
+def receive_signal(callback):
+    sock = bluez.hci_open_dev(0)
+
+    # save current filter
+    old_filter = sock.getsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, 14)
+
+    flt = bluez.hci_filter_new()
+    bluez.hci_filter_all_events(flt)
+    bluez.hci_filter_set_ptype(flt, bluez.HCI_EVENT_PKT)
+    bluez.hci_filter_set_event(flt, EVT_LE_META_EVENT)
+    sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, flt)
+
+    _set_scan_enable(sock, False)
+    _set_scan_parameters(sock)
+    _set_scan_enable(sock, True)
+
+    transmission_id = None
+
+    while True:
+        pkt = sock.recv(255)
+        ptype, event, plen = struct.unpack("BBB", pkt[:3])
+        if event == EVT_LE_META_EVENT:
+            subevent, num = struct.unpack("BB", pkt[3:5])
+            if subevent == EVT_LE_ADVERTISING_REPORT and num == 1:
+                etype, dlen = struct.unpack("BxxxxxxxB", pkt[5:14])
+                if etype == ADV_SCAN_IND and dlen >= 8:
+                    data = pkt[14:-1]
+                    if data[:3] == b'\xff\x03\x97':
+                        tid, hash = struct.unpack("<xxxBL", data[:8])
+                        value = data[8:].decode()
+                        if tid != transmission_id:
+                            callback(hash, value)
+                            transmission_id = tid
+
+    # restore old filter
+    sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, old_filter)
+
 if __name__ == "__main__":
     import sys
-    transmit_signal(int(sys.argv[1]), sys.argv[2], sys.argv[3])
+    if sys.argv[1] == 'transmit':
+        transmit_signal(int(sys.argv[2]), sys.argv[3], sys.argv[4])
+    elif sys.argv[1] == 'receive':
+        def r(hash, value):
+            print(hash, value)
+        receive_signal(r)
