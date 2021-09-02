1631eb76ad48147cb384930efe2ff8e2536c3146
Shinichiro Oba <bricklife@gmail.com>
2021-09-02 23:39:16 +0900

    Add receiver script for SPIKE Prime Hub

---
+++ spike-prime/receiver.py
@@ -0,0 +1,44 @@
+from spike import PrimeHub
+from spike.control import wait_for_seconds
+import ubluetooth
+import ustruct
+#import binascii
+from micropython import const
+
+hub = PrimeHub()
+ble = ubluetooth.BLE()
+transmission_id = None
+
+#signal_hash = binascii.crc32("ABC".encode())
+
+_IRQ_SCAN_RESULT    = const(5)
+_IRQ_SCAN_DONE      = const(6)
+
+def receive_signal(duration_ms, callback):
+    def _bt_irq(event, data):
+        global transmission_id
+        if event == _IRQ_SCAN_RESULT:
+            addr_type, addr, adv_type, rssi, adv_data = data
+            if adv_type == 0x02 and len(adv_data) >= 8 and adv_data[:3] == b'\xff\x03\x97':
+                tid, hash = ustruct.unpack("<BL", adv_data[3:8])
+                value = adv_data[8:].decode()
+                if tid != transmission_id:
+                    callback(hash, value, False)
+                    transmission_id = tid
+        elif event == _IRQ_SCAN_DONE:
+            callback(None, None, True)
+
+    ble.active()
+    ble.irq(_bt_irq)
+    ble.gap_scan(duration_ms, 10000, 10000)
+
+def _receive(hash, value, done):
+    #if hash == signal_hash:
+    if value:
+        hub.light_matrix.write(value)
+    if done:
+        hub.light_matrix.show_image('ASLEEP')
+        wait_for_seconds(1)
+        hub.light_matrix.off()
+
+receive_signal(10 * 1000, _receive)
